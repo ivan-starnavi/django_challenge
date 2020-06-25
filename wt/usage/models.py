f229bd031d3eb8af340c717531a1e7fdf3e6c5a8
Ivan Podkidyshev <raashicat@gmail.com>
2020-06-25 12:52:17 +0400

    added abstract model for subscrion usage. ignoring sqlite db

--- wt/usage/models.py
+++ wt/usage/models.py
@@ -4,19 +4,22 @@ from wt.att_subscriptions.models import ATTSubscription
 from wt.sprint_subscriptions.models import SprintSubscription
 
 
-class DataUsageRecord(models.Model):
-    """Raw data usage record for a subscription"""
+class UsageRecord(models.Model):
+    """Abstract model for subscription usage"""
     att_subscription_id = models.ForeignKey(ATTSubscription, null=True, on_delete=models.PROTECT)
     sprint_subscription_id = models.ForeignKey(SprintSubscription, null=True, on_delete=models.PROTECT)
     price = models.DecimalField(decimal_places=2, max_digits=5, default=0)
     usage_date = models.DateTimeField(null=True)
+
+    class Meta:
+        abstract = True
+
+
+class DataUsageRecord(UsageRecord):
+    """Raw data usage record for a subscription"""
     kilobytes_used = models.IntegerField(null=False)
 
 
-class VoiceUsageRecord(models.Model):
+class VoiceUsageRecord(UsageRecord):
     """Raw voice usage record for a subscription"""
-    att_subscription_id = models.ForeignKey(ATTSubscription, null=True, on_delete=models.PROTECT)
-    sprint_subscription_id = models.ForeignKey(SprintSubscription, null=True, on_delete=models.PROTECT)
-    price = models.DecimalField(decimal_places=2, max_digits=5, default=0)
-    usage_date = models.DateTimeField(null=True)
     seconds_used = models.IntegerField(null=False)
