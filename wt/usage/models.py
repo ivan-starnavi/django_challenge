f28c6712f3f2b64b18723edbb6d42558818a2406
Ivan Podkidyshev <raashicat@gmail.com>
2020-06-25 13:21:19 +0400

    completed 1st task - created models for aggregated usage

--- wt/usage/models.py
+++ wt/usage/models.py
@@ -4,6 +4,10 @@ from wt.att_subscriptions.models import ATTSubscription
 from wt.sprint_subscriptions.models import SprintSubscription
 
 
+# ---------------------------
+# Abstracts
+# ---------------------------
+
 class UsageRecord(models.Model):
     """Abstract model for subscription usage"""
     att_subscription_id = models.ForeignKey(ATTSubscription, null=True, on_delete=models.PROTECT)
@@ -15,6 +19,20 @@ class UsageRecord(models.Model):
         abstract = True
 
 
+class AggregatedUsageRecord(models.Model):
+    """Abstract model for aggregated subscription usage by date"""
+    sum_price = models.DecimalField(decimal_places=2, max_digits=10, default=0)
+    usage_date = models.DateField()
+
+    class Meta:
+        abstract = True
+
+
+# ---------------------------
+# Implementations
+# ---------------------------
+
+
 class DataUsageRecord(UsageRecord):
     """Raw data usage record for a subscription"""
     kilobytes_used = models.IntegerField(null=False)
@@ -23,3 +41,19 @@ class DataUsageRecord(UsageRecord):
 class VoiceUsageRecord(UsageRecord):
     """Raw voice usage record for a subscription"""
     seconds_used = models.IntegerField(null=False)
+
+
+class AggregatedDataUsageRecord(AggregatedUsageRecord):
+    """Aggregated data usage record for subscriptions by date"""
+    sum_kilobytes_used = models.IntegerField(default=0)
+
+    class Meta:
+        db_table = 'agg_data_usage'
+
+
+class AggregatedVoiceUsageRecord(AggregatedUsageRecord):
+    """Aggregated voice usage record for subscriptions by date"""
+    sum_seconds_used = models.IntegerField(default=0)
+
+    class Meta:
+        db_table = 'agg_voice_usage'
